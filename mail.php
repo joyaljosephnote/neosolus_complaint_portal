826e64b6fc5788c561c8d166d5741d6982116e58
Joyal Joseph <joyaljosephnote@gmail.com>
2024-02-14 17:46:07 +0530

    Email function locally work successfully

--- mail.php
+++ mail.php
@@ -1,20 +1,21 @@
 <?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+  $name = $_POST['w3lName'];
+  $mobileNumber = $_POST['w3lMobileNumber'];
+  $senderEmail = $_POST['w3lSender'];
+  $assetId = $_POST['w3lassetId'];
+  $message = $_POST['w3lMessage'];
+  $recipient = "neosolus2023@gmail.com";
+  $mailheader = "From: $name";
+  $combinedMessage = "Asset ID: $assetId\n\nSender Email: $senderEmail\n\nMessage: $message";
 
-$name = $_POST['w3lName'];
-// $subject = "Complaint Registration";
-$mobileNumber = $_POST['w3lMobileNumber'];
-$senderEmail = $_POST['w3lSender'];
-$assetId = $_POST['w3lassetId'];
-$message = $_POST['w3lMessage'];
 
-$mailheader = "Form:".$name."<".$senderEmail.">\r\n"
-
-$recipient = "neosolus2023@gmail.com";
-
-mail($recipient, $mobileNumber, $message, $mailheader)
-
-or die("Error!");
-
-echo "Email sent";
-
+  if(mail($recipient,$name,$combinedMessage,$mailheader)){
+    header("Location: success.html");
+        exit();
+  }else{
+    header("Location: page-error.html");
+        exit();
+  } 
+}
 ?>
